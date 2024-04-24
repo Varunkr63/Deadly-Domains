2f43d0fd28ddefe61476584e6e249eac17709cbc
Varun Kumar <varunkumar63931@gmail.com>
2024-04-24 19:10:10 +0530

    adding plan model

---
+++ backend/Models/planModel.js
@@ -0,0 +1,28 @@
+const { model, Schema } = require('../connection');
+
+const mySchema = new Schema({
+    title: String,
+    pricing: String,
+    provider: String,
+     
+    backup: {type:Storage},
+    cloudhosting: String,
+    managedhosting: String,
+    customersupport: String,
+    features: Array,
+    ddosProtection: String,
+    malwareScanner: String,
+    freeCDN: String,
+    unlimitedfreeSSL: String,
+    GITaccess: String,
+    
+    uptimegurantee: String
+
+
+});
+
+
+
+
+
+module.exports = model('plan', mySchema);
