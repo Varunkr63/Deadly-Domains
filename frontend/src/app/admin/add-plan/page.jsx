fedd9ff7ba96f0c2f3451fd9ec62327381c4310c
Varun Kumar <varunkumar63931@gmail.com>
2024-04-25 18:51:07 +0530

    manage user

--- frontend/src/app/admin/add-plan/page.jsx
+++ frontend/src/app/admin/add-plan/page.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react'
 import { useFormik } from 'formik'
 import { enqueueSnackbar } from 'notistack'
@@ -32,7 +33,7 @@ const AddPlan = () => {
             console.log(values);
 
             //sending request to backend
-            fetch('http://localhost:5000/addplan/add', {
+            fetch('http://localhost:5000/plan/add', {
                 method: 'POST',
                 body: JSON.stringify(values),//convert js to json
                 headers: {
@@ -42,7 +43,7 @@ const AddPlan = () => {
                 .then((response) => {
                     console.log(response.status);
                     if (response.status === 200) {
-                        enqueueSnackbar("feedback added Successfully", { variant: "success" })
+                        enqueueSnackbar("Paln added Successfully", { variant: "success" })
 
                     } else {
                         enqueueSnackbar("Something went worng", { variant: "warning" })
@@ -59,7 +60,231 @@ const AddPlan = () => {
 
     return (
         <div>
-           
+           <div className="bg-white py-6 sm:py-8 lg:py-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+    {/* text - start */}
+    <div className="mb-10 md:mb-16">
+      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+        Add Plans
+      </h2>
+      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+       
+      </p>
+    </div>
+    {/* text - end */}
+    {/* form - start */}
+    <form className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2" onSubmit={addplanForm.handleSubmit}>
+      <div>
+        <label
+          htmlFor="first-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+        Title*
+        </label>
+        <input
+          name="title"
+          id='title'
+          value={addplanForm.values.title}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="first-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+        Free CDN*
+        </label>
+        <input
+          name="freeCDN"
+          id='freeCDN'
+          value={addplanForm.values.freeCDN}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="first-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+        Cloud Hosting*
+        </label>
+        <input
+          name="cloudhosting"
+          id='cloudhosting'
+          value={addplanForm.values.cloudhosting}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="last-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Manage Hosting*
+        </label>
+        <input
+          name="managedhosting"
+          id='managedhosting'
+          value={addplanForm.values.managedhosting}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="first-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+        Features*
+        </label>
+        <input
+          name="features"
+          id='features'
+          value={addplanForm.values.features}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="last-name"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          DDOS Protection*
+        </label>
+        <input
+          name="ddosProtection"
+          id='ddosProtection'
+          value={addplanForm.values.ddosProtection}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="text"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+         Provider
+        </label>
+        <input
+          name="provider"
+          id='provider'
+          value={addplanForm.values.provider}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="number"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Backup*
+        </label>
+        <input
+          name="backup"
+          id='backup'
+          value={addplanForm.values.backup}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="subject"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Customer Support*
+        </label>
+        <input
+          name="customersupport"
+          id='customersupport'
+          value={addplanForm.values.customersupport}
+          onChange={addplanForm.handleChange}
+          className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Malware Scanner*
+        </label>
+        <input
+          name="malwareScanner"
+          id='malwareScanner'
+          value={addplanForm.values.malwareScanner}
+          onChange={addplanForm.handleChange}
+          className=" w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          defaultValue={""}
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Unlimited FreeSSL*
+        </label>
+        <input
+          name="unlimitedfreeSSL"
+          id='unlimitedfreeSSL'
+          value={addplanForm.values.unlimitedfreeSSL}
+          onChange={addplanForm.handleChange}
+          className=" w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          defaultValue={""}
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          GIT Access*
+        </label>
+        <input
+          name="GITaccess"
+          id='GITaccess'
+          value={addplanForm.values.GITaccess}
+          onChange={addplanForm.handleChange}
+          className=" w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          defaultValue={""}
+        />
+      </div>
+      <div className="sm:col-span-2">
+        <label
+          htmlFor="message"
+          className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+        >
+          Up Time Gurantee*
+        </label>
+        <input
+          name="uptimegurantee"
+          id='uptimegurantee'
+          value={addplanForm.values.uptimegurantee}
+          onChange={addplanForm.handleChange}
+          className=" w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+          defaultValue={""}
+        />
+      </div>
+      <div className="flex items-center justify-between sm:col-span-2">
+        <button className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
+          Send
+        </button>
+        
+      </div>
+     
+    </form>
+    {/* form - end */}
+  </div>
+</div>
+
            
             
         </div>
