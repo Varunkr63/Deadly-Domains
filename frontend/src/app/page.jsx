1bec74b29db0333ee275fa89c324bfb4198910a0
m-triple-m <triplem656@gmail.com>
2024-05-15 15:51:14 +0530

    ..

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -56,9 +56,9 @@ const Home = () => {
               <div className="text-center">
                 <Link
                   className="mr-5 inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800"
-                  href={"/signup"}
+                  href={"/user/check-domain"}
                 >
-                  Signup
+                  Find Domain
                   <svg
                     className="flex-shrink-0 size-4"
                     xmlns="http://www.w3.org/2000/svg"
